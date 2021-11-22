124ad1dc58d52fff3d09f4754145f0ac3827de9f
Jimmy Kiarie <jimmy.kiarie@student.moringaschool.com>
2021-11-22 04:32:58 +0300

    add js file and grading html

---
+++ js/grading.js
@@ -0,0 +1,20 @@
+var grade = prompt("Enter the Score: ");
+if (grade >= 80 && grade <= 100)
+{
+  alert("Your Grade is: A");
+}
+else if (grade <= 79 && grade >= 60)
+{
+  alert("Your Grade is: B");
+}
+else if (grade <= 59 && grade >= 40)
+{
+  alert("Your Grade is: C");
+}
+else if (grade <= 39 && grade >= 20)
+{
+  alert("Your Grade is: D");
+}
+else{
+ alert("Your grade is: E")
+}
